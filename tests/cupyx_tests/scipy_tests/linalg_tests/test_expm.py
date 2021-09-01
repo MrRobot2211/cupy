7f2f15ebb6d546acd32f21234b05b915df36e43a
MrRobot2211 <felipebihaiek@gmail.com>
2021-09-01 02:15:04 -0300

    fromat skptest string

--- tests/cupyx_tests/scipy_tests/linalg_tests/test_expm.py
+++ tests/cupyx_tests/scipy_tests/linalg_tests/test_expm.py
@@ -28,7 +28,10 @@ class TestLUFactor(unittest.TestCase):
     @testing.for_dtypes("fdFD")
     def test_lu_factor(self, dtype):
         if self.shape[0] != self.shape[1]:
-            self.skipTest("skip non-square tests since scipy.expm requires square")
+            self.skipTest(
+                """skip non-square tests 
+                            since scipy.expm requires square"""
+            )
         a_cpu = testing.shaped_random(self.shape, numpy, dtype=dtype)
         a_gpu = cupy.asarray(a_cpu)
         result_cpu = scipy.linalg.expm(a_cpu)
